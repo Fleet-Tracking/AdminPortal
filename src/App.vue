09fd37efe3783b548d94599c7425fd8bc9523684
ovenoboyo <ovenoboyo@gmail.com>
2021-10-21 20:51:41 +0530

    Initialize firebase after vue

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,20 @@
 </template>
 
 <script lang="ts">
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 import { Component, Vue } from "vue-property-decorator";
+import { vxm } from "./store";
+import { firebaseConfig } from "./utils/constants";
 
 @Component({
   name: "App",
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  mounted(): void {
+    vxm.firebase.app = initializeApp(firebaseConfig);
+    vxm.firebase.db = getDatabase(vxm.firebase.app);
+  }
+}
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,9 @@ import Vue from "vue";
 import App from "@/App.vue";
 import "@/registerServiceWorker";
 import router from "@/router/index";
-import { store, vxm } from "@/store";
+import { store } from "@/store";
 import "@/assets/css/styles.scss";
 import './plugins/bootstrap'
-
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-import { firebaseConfig } from "./utils/constants";
 
 Vue.prototype.$mapFields = Vue;
 
@@ -20,9 +16,6 @@ new Vue({
   render: (h) => h(App),
 }).$mount("#app");
 
-vxm.firebase.app = initializeApp(firebaseConfig);
-
-vxm.firebase.db = getDatabase(vxm.firebase.app);
 
 
 
